1dacc3ab1fd73938ae9c888c78e3766e42561d98
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2025-01-14 21:15:12 -0300

    simple build to pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import path from "path";
 
-const root = "./src/pages/"
+const root = "./src/pages/";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
   root,
   build: {
-    target: 'esnext',
+    target: "esnext",
     emptyOutDir: true,
-    assetsDir: 'assets',
+    assetsDir: "assets",
     rollupOptions: {
       output: {
-        dir: 'build',
+        dir: "build",
       },
       input: {
-        home: path.join(root, 'index.html'),
-
-      }
-    }
-  }
-})
+        home: path.join(root, "index.html"),
+        youtubeClone: path.join(root, "youtube-clone", "/index.html"),
+      },
+    },
+  },
+});
