abd3cb6c28f4ab030ac9786aac8e0768325bf963
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2025-01-17 13:07:13 -0300

    moved the locales json to a specified page, also new vite configs to guarantee the public page

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,21 @@ import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import path from "path";
 
-const root = "./src/pages/";
+const root = path.resolve(__dirname, "./src/pages");
+const publicDir = path.resolve(__dirname, "public");
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
   root,
+  publicDir,
   build: {
     target: "esnext",
     emptyOutDir: true,
     assetsDir: "assets",
     rollupOptions: {
       output: {
-        dir: "build",
+        dir: path.resolve(__dirname, "build"),
       },
       input: {
         home: path.join(root, "index.html"),
