64de151150f76afb89e7c45af9f7d672c7cb3a08
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2025-01-17 13:08:11 -0300

    added cacheing to web fetcher

--- src/libs/WebFetcher.ts
+++ src/libs/WebFetcher.ts
@@ -12,28 +12,75 @@ export type WebFetcherConfig = {
     target: "json" | "text" | "blob"
 }
 
-const defaultConfig : WebFetcherConfig = {
+const defaultConfig: WebFetcherConfig = {
     cache: true,
-    cacheTime: 1000 * 60 * 30, // 30 minutes
+    cacheTime: 1000 * 60 * 30, // 30 minutes by default
     cacheStorage: "Default",
     target: "json"
 }
 
-export const WebFetcher = async (url : string, config : WebFetcherConfig = defaultConfig) => {
-    
+export const WebFetcher = async (url: string, configurations: Partial<WebFetcherConfig> = defaultConfig) => {
+    const config: WebFetcherConfig = { ...defaultConfig, ...configurations }
+
     const cacheStorage = await caches.open(config.cacheStorage);
     const cachedResult = await cacheStorage.match(url);
-    
-    if (cachedResult) { 
-        const cacheData = await cachedResult.json();
-        return cacheData;
-    } 
+
+    if (cachedResult) {
+        const cachedTimestamp = cachedResult.headers.get("X-Timestramp");
+
+        if (cachedTimestamp && (parseInt(cachedTimestamp) < (new Date().getTime()))) {
+
+            let data = undefined
+            switch (config.target) {
+                case "json":
+                    data = await cachedResult.json();
+                    break;
+                case "text":
+                    data = await cachedResult.text();
+                    break;
+                case "blob":
+                    data = await cachedResult.blob();
+                    break;
+            }
+            return data
+        } else {
+            // deletes the stale data
+            await cacheStorage.delete(url)
+        }
+    }
+
+
 
     // if the data is not already in storaged (or failed to fetch), fetch it from the web
-    const fetchData = await fetch(url);
+    const fetchedData = await fetch(url);
 
+    if (fetchedData.status >= 200 && fetchedData.status <= 400) {
 
+        let data = undefined
+        // saves the request on the cache.
+        let response = fetchedData.clone();
+        const header = new Headers(response.headers)
+        header.set("X-Timestamp", String(new Date().getTime() + config.cacheTime) )
+        response = new Response(response.body, {headers: header});
 
+        if (config.cache) {
+            await cacheStorage.put(url, response)
+        }
+        
+        switch (config.target) {
+            case "json":
+                data = await fetchedData.json();
+                break;
+            case "text":
+                data = await fetchedData.text();
+                break;
+            case "blob":
+                data = await fetchedData.blob();
+                break;
+        }
 
-    return fetchData
+        return data
+    }
+
+    return fetchedData
 }
