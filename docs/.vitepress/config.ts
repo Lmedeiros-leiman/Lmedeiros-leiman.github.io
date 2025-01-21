30332c538c245ef5df09d5ac9c7228ac7e73c3fc
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2025-01-21 10:27:49 -0300

    adicionado base nas config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/blog/",
+  base: "/portifolio/blog/",
   outDir : "./../build/blog",
 
   title: "Leo blog",
